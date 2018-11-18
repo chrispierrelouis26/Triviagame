4b2925027efd36f66c7ba6abc2ea6e42f79b82f0
chris pierre <lilchr@yahoo.com>
2018-11-17 16:39:33 -0800

    saturdays add ons

--- assets/javascript/app.js
+++ assets/javascript/app.js
@@ -2,8 +2,13 @@ console.log("hey there");
 
 $(document).ready(function(){
 
+  
+// create variables for correct/wrong answers
 
 var time = 100;
+var correctAnswers = 0;
+var wrongAnswers = 0;
+var totalAnswers = 0;
 
 
  function start() {
@@ -24,58 +29,135 @@ var time = 100;
     $("#timer").text(converted);
   }
 
-  start();
+  // start();
 
-   function timeConverter(t) {
+  //  function timeConverter(t) {
 
-    var minutes = Math.floor(t / 60);
-    var seconds = t - (minutes * 60);
+  //   var minutes = Math.floor(t / 60);
+  //   var seconds = t - (minutes * 60);
 
-    if (seconds < 10) {
-      seconds = "0" + seconds;
-    }
+  //   if (seconds < 10) {
+  //     seconds = "0" + seconds;
+  //   }
 
-    if (minutes === 0) {
-      minutes = "00";
-    }
-    else if (minutes < 10) {
-      minutes = "0" + minutes;
-    }
-    else if ( seconds < 0 && minutes !=0){
-      minutes -=1;
-      seconds=59;
-    }
+  //   if (minutes === 0) {
+  //     minutes = "00";
+  //   }
+  //   else if (minutes < 10) {
+  //     minutes = "0" + minutes;
+  //   }
+  //   else if ( seconds < 0 && minutes !=0){
+  //     minutes -=1;
+  //     seconds=59;
+  //   }
 
-    return minutes + ":" + seconds;
-  }
+  //   return minutes + ":" + seconds;
+  // }
 
-  function stop() {
-    clearInterval(intervalId);
-    count = false;
+  // function stop() {
+  //   clearInterval(intervalId);
+  //   count = false;
     
-  }
+  // }
 
+  
 
-    $("#button").on("click" , function(){
+    $("#submit").on("click" , function(){
+      event.preventDefault();
       console.log("good morning!");
 
-   
+      // function for when correct answer/wrong answer is chosen
+      //first question
+      $.each($("input[name='q-0']:checked"),function(){
+        console.log($(this).val());
+        
+        if($(this).val()===questions.Q1.Answer){
+      
+          correctAnswers ++;
+        
+        }
+         
+        else {
+          alert("incorrect answer");
+          wrongAnswers ++;
+        
+        }
+        
 
+      })
+      // second question
+      $.each($("input[name='q-1']:checked"),function(){
+        console.log($(this).val());
+
+        if($(this).val()===questions.Q4.Answer){
+       
+
+          
+        }
+         
+        else {
+          wrongAnswers ++;
+          alert(wrongAnswers);
+        }
+        
+
+      })
+
+      // third question
+      $.each($("input[name='q-2']:checked"),function(){
+        console.log($(this).val());
+
+        if($(this).val()===questions.Q2.Answer){
+          correctAnswers ++;
+        }
+         
+        else {
+          wrongAnswers ++;
+        }
+        
+
+      })
+
+      //fourth question
+      $.each($("input[name='q-3']:checked"),function(){
+        console.log($(this).val());
+
+        if($(this).val()===questions.Q5.Answer){
+          correctAnswers ++; 
+        }
+         
+        else {
+          wrongAnswers ++;
+    
+        }
+
+      })
+
+      if (correctAnswers > wrongAnswers){
+        totalAnswers++;
+        alert(totalAnswers);
+      }
+      else{
+        console.log("nothing");
+      }
   });
 
 
-  $("#button1").on("click" ,function(){
-    alert("you pressed me!");
-  });
+
+
+  
+  // $("#button1").on("click" ,function(){
+  //   alert("you pressed me!");
+  // });
   
   
-  $("#button3").on("click" ,function(){
-    alert("Thats correct!");
-  });
+  // $("#button3").on("click" ,function(){
+  //   alert("Thats correct!");
+  // });
   
-  $(".q-0").on("click", function(){
-    console.log($(this).text());
-  });
+  // $(".q-0").on("click", function(){
+  //   console.log($(this).text());
+  // });
 // Array of questions
 
 
@@ -88,8 +170,8 @@ var questions = {
 
   Q2: {
   Question2: "What is my last name?"  ,
-  Options: ["Pierre", "Smith", "Doe", "Adams"],
-  Answer: "Pierre"
+  Options: ["christopher", "Smith", "Doe", "Adams"],
+  Answer: "christopher"
  },
 
 Q3: {
@@ -101,19 +183,27 @@ Q3: {
 Q4:{
   Question4: "What is the largest ocean in the world?" ,
   Options: [ "Pacific","Atlantic" , "Arctic", "Oceanic"],
-  Answer : "Pacific"
+  Answer : "Atlantic"
+},
+
+Q5:{
+  Question5: "What is the capital of new mexico?" ,
+  Options: [ "Charlotte","Albany" , "Sante Fe"],
+  Answer : "Sante Fe"
 }
 
 };
+
+
+
+
+
 console.log(questions);
 console.log(questions.Q1.Question);
 
 for (i = 0; i < questions.length; i++) { 
   
 }
-// create variables for correct/wrong answers
-var correctAnswers = 0;
-var wrongAnswers = 0;
 
  $("#donald").on("click" , function(){
    console.log("horray!");
